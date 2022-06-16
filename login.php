743cf9824337386b4a63286fb5c732d0b8ff5311
Ruben Rooms <ruben.rooms@endare.com>
2022-06-16 18:39:13 +0200

    login function

--- login.php
+++ login.php
@@ -1,3 +1,27 @@
+<?php
+
+    include_once("classes/Db.php");
+    include_once("classes/User.php");
+
+
+        if (!empty($_POST)) {
+            $user = new User();
+            $user->setUsername($_POST['username']);
+            $user->setPassword($_POST['password']);
+
+        try{
+            if($user->canLogin()){
+                session_start();
+                $_SESSION['username'] = $_POST['username'];
+                $_SESSION['id'] = $user->getId();
+                header("Location: index.php");
+            }  
+        } catch (\Throwable $th) {
+            $error = $th->getMessage();
+        }
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
