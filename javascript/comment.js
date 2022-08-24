6ea2f8360cca867296751ce44a7c4fce14f0e317
Ruben Rooms <ruben.rooms@endare.com>
2022-08-24 23:13:23 +0200

    added class to comments fro the right styling

--- javascript/comment.js
+++ javascript/comment.js
@@ -20,6 +20,7 @@ document.querySelector("#commentBtn").addEventListener("click", function () {
         .then((response) => response.json())
         .then((result) => {
             let newComment = document.createElement("li");
+            newComment.className = "py-2 px-2 rounded-3 bg-light list-group-item"
             newComment.innerHTML = result.body;
             document
                 .querySelector(".commentsOnTodo")
